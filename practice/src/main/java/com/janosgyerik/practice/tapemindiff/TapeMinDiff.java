7d54e3e9e346c8fdcbf0587375d2d547e22630e6
Janos Gyerik <info@janosgyerik.com>
2014-02-27 21:01:15 +0100

    added tapemindiff tests

---
+++ practice/src/main/java/com/janosgyerik/practice/tapemindiff/TapeMinDiff.java
@@ -0,0 +1,11 @@
+package com.janosgyerik.practice.tapemindiff;
+
+public class TapeMinDiff {
+    public int minDiff(int[] arr) {
+        return 0;
+    }
+
+    public int submit(int[] arr) {
+        return minDiff(arr);
+    }
+}
---
+++ practice/src/test/java/com/janosgyerik/practice/tapemindiff/TapeMinDiffTest.java
@@ -0,0 +1,47 @@
+package com.janosgyerik.practice.tapemindiff;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TapeMinDiffTest {
+
+    @Test
+    public void test_diff_1_2_is_1() {
+        Assert.assertEquals(1, submit(new int[]{1, 2}));
+    }
+
+    @Test
+    public void test_diff_1_3_is_2() {
+        Assert.assertEquals(2, submit(new int[]{1, 3}));
+    }
+
+    @Test
+    public void test_diff_2_1_is_1() {
+        Assert.assertEquals(1, submit(new int[]{2, 1}));
+    }
+
+    @Test
+    public void test_diff_1_2_1_is_1() {
+        Assert.assertEquals(1, submit(new int[]{1, 2, 1}));
+    }
+
+    @Test
+    public void test_diff_1_2_3_is_1() {
+        Assert.assertEquals(1, submit(new int[]{1, 2, 3}));
+    }
+
+    @Test
+    public void test_diff_7_2_3_is_1() {
+        Assert.assertEquals(1, submit(new int[]{7, 2, 3}));
+    }
+
+    @Test
+    public void test_diff_3_1_2_4_3_is_1() {
+        Assert.assertEquals(1, submit(new int[]{3, 1, 2, 4, 3}));
+    }
+
+    private int submit(int[] arr) {
+        return new TapeMinDiff().submit(arr);
+    }
+}
