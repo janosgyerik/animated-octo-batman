ba924bdd1a2748da09cfb2ab51bdcab5df22f259
Janos Gyerik <info@janosgyerik.com>
2014-02-27 21:27:34 +0100

    solved

--- practice/src/main/java/com/janosgyerik/practice/tapemindiff/TapeMinDiff.java
+++ practice/src/main/java/com/janosgyerik/practice/tapemindiff/TapeMinDiff.java
@@ -2,7 +2,20 @@ package com.janosgyerik.practice.tapemindiff;
 
 public class TapeMinDiff {
     public int minDiff(int[] arr) {
-        return 0;
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        int diff = sum;
+        for (int i = 0; i < arr.length - 1; ++i) {
+            int newdiff = Math.abs(diff - 2 * arr[i]);
+            if (newdiff < diff) {
+                diff = newdiff;
+            } else {
+                break;
+            }
+        }
+        return diff;
     }
 
     public int submit(int[] arr) {
--- practice/src/test/java/com/janosgyerik/practice/tapemindiff/TapeMinDiffTest.java
+++ practice/src/test/java/com/janosgyerik/practice/tapemindiff/TapeMinDiffTest.java
@@ -23,22 +23,42 @@ public class TapeMinDiffTest {
 
     @Test
     public void test_diff_1_2_1_is_1() {
-        Assert.assertEquals(1, submit(new int[]{1, 2, 1}));
+        Assert.assertEquals(2, submit(new int[]{1, 2, 1}));
     }
 
     @Test
     public void test_diff_1_2_3_is_1() {
-        Assert.assertEquals(1, submit(new int[]{1, 2, 3}));
+        Assert.assertEquals(0, submit(new int[]{1, 2, 3}));
     }
 
     @Test
     public void test_diff_7_2_3_is_1() {
-        Assert.assertEquals(1, submit(new int[]{7, 2, 3}));
+        Assert.assertEquals(2, submit(new int[]{7, 2, 3}));
     }
 
     @Test
     public void test_diff_3_1_2_4_3_is_1() {
         Assert.assertEquals(1, submit(new int[]{3, 1, 2, 4, 3}));
+    }
+
+    @Test
+    public void test_diff_1_7() {
+        Assert.assertEquals(6, submit(new int[]{1, 7}));
+    }
+
+    @Test
+    public void test_diff_1_1_7() {
+        Assert.assertEquals(5, submit(new int[]{1, 1, 7}));
+    }
+
+    @Test
+    public void test_diff_1_1_1_7() {
+        Assert.assertEquals(4, submit(new int[]{1, 1, 1, 7}));
+    }
+
+    @Test
+    public void test_diff_1_1_1_1_7() {
+        Assert.assertEquals(3, submit(new int[]{1, 1, 1, 1, 7}));
     }
 
     private int submit(int[] arr) {
