196dadc708423973f7353b87e0179fc275b2134b
Janos Gyerik <info@janosgyerik.com>
2014-03-03 20:55:45 +0100

    added brackets with test

---
+++ practice/src/main/java/com/janosgyerik/practice/easy/Brackets.java
@@ -0,0 +1,45 @@
+package com.janosgyerik.practice.easy;
+
+import java.util.Stack;
+
+public class Brackets {
+    public int submit(String s) {
+        if (s.isEmpty()) {
+            return 1;
+        }
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            switch (c) {
+                case '{':
+                case '(':
+                case '[':
+                    stack.push(c);
+                    break;
+                case ']':
+                case ')':
+                case '}':
+                    if (stack.isEmpty()) {
+                        return 0;
+                    }
+                    char opening = stack.pop();
+                    if (!isMatchingClose(opening, c)) {
+                        return 0;
+                    }
+            }
+        }
+        return stack.isEmpty() ? 1 : 0;
+    }
+
+    private boolean isMatchingClose(char opening, char closing) {
+        switch (opening) {
+            case '(':
+                return closing == ')';
+            case '{':
+                return closing == '}';
+            case '[':
+                return closing == ']';
+        }
+        return false;
+    }
+}
