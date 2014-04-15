5522f2443ec419225e4c4ebfa823c30197a2aca4
Janos Gyerik <janos@kronos>
2014-04-15 22:43:18 +0200

    added longest prefix test

---
+++ practice/src/test/java/com/janosgyerik/practice/so/LongestCommonPrefixTest.java
@@ -0,0 +1,24 @@
+package com.janosgyerik.practice.so;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LongestCommonPrefixTest {
+    @Test
+    public void testExample() {
+        Assert.assertEquals("aa", findLongestCommonPrefix(new String[]{"aa", "aab", "aaasdd"}));
+        Assert.assertEquals("", findLongestCommonPrefix(new String[]{"apple", "pear"}));
+        Assert.assertEquals("", findLongestCommonPrefix(new String[]{"apple", "pear", "aaa"}));
+    }
+
+    private String findLongestCommonPrefix(String[] strings) {
+        String longest = strings[0];
+        int length = longest.length();
+        for (String str : strings) {
+            while (!str.startsWith(longest)) {
+                longest = longest.substring(0, --length);
+            }
+        }
+        return longest;
+    }
+}
