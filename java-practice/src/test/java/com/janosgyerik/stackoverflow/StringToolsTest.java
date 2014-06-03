286a03f5d9d0ff74f8187ce5da375e3576b1bcd6
Janos Gyerik <janos@kronos>
2014-06-03 23:57:26 +0200

    generalized StringTools.join to Iterable<?>

--- java-practice/src/test/java/com/janosgyerik/stackoverflow/StringToolsTest.java
+++ java-practice/src/test/java/com/janosgyerik/stackoverflow/StringToolsTest.java
@@ -4,22 +4,24 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 
+// TODO move to java-examples
 public class StringToolsTest {
 
-    private String join(Collection<? extends Object> items, String sep) {
-        if (items.isEmpty()) {
+    private String join(Iterable<?> items, String sep) {
+        Iterator<?> iter = items.iterator();
+        if (!iter.hasNext()) {
             return "";
         }
+
         StringBuilder builder = new StringBuilder();
-        Iterator<? extends Object> iter = items.iterator();
         builder.append(iter.next());
         while (iter.hasNext()) {
             builder.append(sep).append(iter.next());
         }
+
         return builder.toString();
     }
 
@@ -35,9 +37,18 @@ public class StringToolsTest {
     }
 
     @Test
-    public void testJoinTwoItems() {
-        String item1 = "hello";
-        String item2 = "hi";
+    public void testJoinStrings() {
+        Object item1 = "hello";
+        Object item2 = "there";
+        String sep = ", ";
+        String expected = item1 + sep + item2;
+        Assert.assertEquals(expected, join(Arrays.asList(item1, item2), sep));
+    }
+
+    @Test
+    public void testJoinNumbers() {
+        Integer item1 = 4;
+        Integer item2 = 9;
         String sep = ", ";
         String expected = item1 + sep + item2;
         Assert.assertEquals(expected, join(Arrays.asList(item1, item2), sep));
