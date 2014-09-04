e564eb36a6e880f8f00728792b31853ff02b56af
Janos Gyerik <janos@kronos>
2014-09-04 20:21:48 +0200

    added more tests for score range

--- java-practice/src/test/java/com/janosgyerik/stackoverflow/user16547/ScoreRangeTest.java
+++ java-practice/src/test/java/com/janosgyerik/stackoverflow/user16547/ScoreRangeTest.java
@@ -1,36 +1,109 @@
 package com.janosgyerik.stackoverflow.user16547;
 
-import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+class ScoreCounter {
+	public int score = 1;
+	private int level = 0;
+	private boolean increased100500;
+	private boolean increased5001500;
+	private boolean increased15003000;
+
+	private final int[] thresholds = new int[]{100, 500, 1500};
+	private final Set<Integer> passedThresholds = new HashSet<>();
+
+	public void increaseScore() {
+		for (int threshold : thresholds) {
+			if (score >= threshold && !passedThresholds.contains(threshold)) {
+				passedThresholds.add(threshold);
+				increase();
+				break;
+			}
+		}
+	}
+
+	public void increaseScore2() {
+		if (score >= 100 && !increased100500) {
+			increase();
+			increased100500 = true;
+		} else if (score >= 500 && !increased5001500) {
+			increase();
+			increased5001500 = true;
+		} else if (score >= 1500 && !increased15003000) {
+			increase();
+			increased15003000 = true;
+		}
+	}
+
+	private void increase() {
+		++level;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+}
 
 public class ScoreRangeTest {
-	private int Score;
-	private boolean Increased100500;
-	private boolean Increased5001500;
-	private boolean Increased15003000;
 
-	@Before
-	public void setUp() {
-		Score = 1;
-	}
-	private void getDifficulty() {
-		if (Score >= 100 && !Increased100500)
-		{
-			Increase();
-			Increased100500 = true;
-		}
-		else if (Score >= 500 && !Increased5001500)
-		{
-			Increase();
-			Increased5001500 = true;
-		}
-		else if (Score >= 1500 && !Increased15003000)
-		{
-			Increase();
-			Increased15003000 = true;
-		}
+	@Test
+	public void testNoIncrease() {
+		ScoreCounter counter = new ScoreCounter();
+		counter.increaseScore();
+		assertEquals(0, counter.getLevel());
 	}
 
-	private void Increase() {
+	@Test
+	public void testIncreaseOnce() {
+		ScoreCounter counter = new ScoreCounter();
+		counter.increaseScore();
+		assertEquals(0, counter.getLevel());
 
+		counter.score = 200;
+		counter.increaseScore();
+		assertEquals(1, counter.getLevel());
+		counter.increaseScore();
+		assertEquals(1, counter.getLevel());
+	}
+
+	@Test
+	public void testIncreaseTwice() {
+		ScoreCounter counter = new ScoreCounter();
+		counter.increaseScore();
+		assertEquals(0, counter.getLevel());
+
+		counter.score = 200;
+		counter.increaseScore();
+		assertEquals(1, counter.getLevel());
+		counter.increaseScore();
+		assertEquals(1, counter.getLevel());
+
+		counter.score = 600;
+		counter.increaseScore();
+		assertEquals(2, counter.getLevel());
+		counter.increaseScore();
+		assertEquals(2, counter.getLevel());
+	}
+
+	@Test
+	public void testIncreaseThrice() {
+		ScoreCounter counter = new ScoreCounter();
+		counter.increaseScore();
+		assertEquals(0, counter.getLevel());
+
+		counter.score = 5200;
+		counter.increaseScore();
+		assertEquals(1, counter.getLevel());
+		counter.increaseScore();
+		assertEquals(2, counter.getLevel());
+		counter.increaseScore();
+		assertEquals(3, counter.getLevel());
+		counter.increaseScore();
+		assertEquals(3, counter.getLevel());
 	}
 }
