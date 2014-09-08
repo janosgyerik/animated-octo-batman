1076f800421ef96036f1728998115369cecf4722
Janos Gyerik <janos@kronos>
2014-09-08 19:57:22 +0200

    more so answers

---
+++ java-practice/src/test/java/com/janosgyerik/stackoverflow/scratch/CategorizerTest.java
@@ -0,0 +1,45 @@
+package com.janosgyerik.stackoverflow.scratch;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategorizerTest {
+	public static String getCategory(Collection obj) {
+		return "Collection";
+	}
+
+	public static String getCategory(String string) {
+		return "String";
+	}
+
+	public static String getCategory(Object obj) {
+		return "Object";
+	}
+
+	@Test
+	public void testString() {
+		assertEquals("String", getCategory("hello"));
+	}
+
+	@Test
+	public void testCollection() {
+		assertEquals("Collection", getCategory(Arrays.asList(3, 4)));
+	}
+
+	@Test
+	public void testObject() {
+		assertEquals("Object", getCategory(3));
+	}
+
+	@Test
+	public void testMix() {
+		Object[] objs = {"hello", 3, Arrays.asList(1, 2, 3)};
+		for (Object o : objs) {
+			assertEquals("Object", getCategory(o));
+		}
+	}
+}
