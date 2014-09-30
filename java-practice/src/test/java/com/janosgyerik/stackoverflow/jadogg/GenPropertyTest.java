04f486b653b070c0af8dade0896f6a937be5e3bc
Janos Gyerik <janos@kronos>
2014-09-30 20:58:39 +0200

    more answers

---
+++ java-practice/src/test/java/com/janosgyerik/stackoverflow/jadogg/GenPropertyTest.java
@@ -0,0 +1,137 @@
+package com.janosgyerik.stackoverflow.jadogg;
+
+import org.junit.Test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+enum JavaDataType {
+
+    STRING(String.class),
+    INTEGER(Integer.class),
+    BOOLEAN(Boolean.class),
+    DOUBLE(Double.class),
+    FLOAT(Float.class),
+    LONG(Long.class);
+
+    private final String value;
+
+    JavaDataType(Class<?> klass) {
+        this.value = klass.getSimpleName();
+    }
+
+    /**
+     * @return string value
+     */
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    private static final Map<String, JavaDataType> STRING_TO_ENUM = new HashMap<>();
+
+    static {
+        for (JavaDataType javaDataType : values()) {
+            STRING_TO_ENUM.put(javaDataType.toString(), javaDataType);
+        }
+    }
+
+    /**
+     * convert String to JavaDataType
+     *
+     * @param type String type
+     * @return JavaDataType or null if not found
+     */
+    public static JavaDataType fromString(String type) {
+        return STRING_TO_ENUM.get(type);
+    }
+}
+
+public class GenPropertyTest {
+    @Test
+    public void testGetLabels() {
+        List<String> labels = new ArrayList<>();
+//        for (JavaDataType javaDataType : JavaDataType.values()) {
+//            labels.add(javaDataType.toString());
+//        }
+        labels.addAll(Arrays.asList(JavaDataType.values()).stream()
+                .map(JavaDataType::toString)
+                .collect(Collectors.toList()));
+        assertEquals(Arrays.asList("String", "Integer", "Boolean", "Double", "Float", "Long"), labels);
+    }
+
+    @Test
+    public void testFromString() {
+        assertEquals(JavaDataType.BOOLEAN, JavaDataType.fromString("Boolean"));
+        assertEquals(JavaDataType.DOUBLE, JavaDataType.fromString("Double"));
+    }
+
+    @Test
+    public void testJavaClassNames() {
+        assertEquals("String", String.class.getSimpleName());
+        assertEquals("Double", Double.class.getSimpleName());
+    }
+
+    @Test
+    public void testFormatStringBuilder() {
+        String code = "import javafx.beans.property.*;\n\n"
+                + "/**\n"
+                + " * Class Information\n"
+                + " * @author Your Name\n"
+                + " */\n"
+                + "public class %s {\n\n"
+                + "%s\n\n"
+                + "    public %s() {\n"
+                + "%s\n"
+                + "    }\n\n"
+                + "%s\n\n"
+                + "}";
+        String placeHolder = ":";
+        StringBuilder builder = new StringBuilder("hello");
+        assertEquals("import javafx.beans.property.*;\n" +
+                "\n" +
+                "/**\n" +
+                " * Class Information\n" +
+                " * @author Your Name\n" +
+                " */\n" +
+                "public class : {\n" +
+                "\n" +
+                ":\n" +
+                "\n" +
+                "    public :() {\n" +
+                ":\n" +
+                "    }\n" +
+                "\n" +
+                ":\n" +
+                "\n" +
+                "}", String.format(code,
+                placeHolder,
+                placeHolder, placeHolder, placeHolder, placeHolder));
+        String hello = null;
+        switch (hello) {
+            case "jack":
+                System.out.println("test1");
+                break;
+            case "something":
+            default:
+                break;
+        }
+    }
+}
+
+class Blah extends HttpServlet {
+    private final Map<Integer, String> products;
+
+    public Blah() {
+        products = new HashMap<>();
+        products.put(1, "Sandpaper");
+        products.put(2, "Nails");
+        products.put(3, "Glue");
+        products.put(4, "Paint");
+        products.put(5, "Tape");
+    }
+}
---
+++ java-practice/src/test/java/com/janosgyerik/stackoverflow/user243872/HiLoTest.java
@@ -0,0 +1,64 @@
+package com.janosgyerik.stackoverflow.user243872;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class Game implements Runnable {
+    private final long gameTime;
+
+    public Game(int gameTime) {
+        this.gameTime = gameTime;
+    }
+
+    public void run() {
+        long start = System.currentTimeMillis();
+        while (true) {
+            long elapsedTime = (System.currentTimeMillis() - start) / 1000;
+            if (elapsedTime >= gameTime) {
+                System.out.println("Oops! Time is up - try again.");
+                break;
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // it's okay, just awakened early
+            }
+        }
+    }
+}
+
+class Hilo {
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Must enter time");
+        }
+        Random rand = new Random();
+        int max = 100;
+        int min = 1;
+        int number = rand.nextInt((max - min) + 1) + min;
+        int gameTime = Integer.parseInt(args[0]);
+        System.out.println("Welcome to HiLo!");
+        System.out.println("You have " + gameTime + " seconds to guess a number between 1 and 100.");
+        Thread clock1 = new Thread(new Game(gameTime));
+        clock1.start();
+
+        while (clock1.isAlive()) {
+            System.out.println(">");
+            Scanner sc = new Scanner(System.in);
+            int input = sc.nextInt();
+
+            if (clock1.isAlive() && input == number) {
+                System.out.println("You Win!");
+                break;
+            } else if (clock1.isAlive() && input < number) {
+                System.out.println("Higher!");
+            } else if (clock1.isAlive() && input > number) {
+                System.out.println("Lower!");
+            }
+        }
+    }
+}
+
+public class HiLoTest {
+}
