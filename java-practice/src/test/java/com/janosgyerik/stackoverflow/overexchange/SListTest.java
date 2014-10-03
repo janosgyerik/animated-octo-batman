3ad5886ef9166998ca067099f75617da16592104
Janos Gyerik <janos@kronos>
2014-10-03 21:55:11 +0200

    more answers

---
+++ java-practice/src/test/java/com/janosgyerik/stackoverflow/overexchange/SListTest.java
@@ -0,0 +1,199 @@
+package com.janosgyerik.stackoverflow.overexchange;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+class SList {
+    private SListNode head;
+    private int size;
+
+    /**
+     * SList() constructs an empty list.
+     */
+
+    public SList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    /**
+     * isEmpty() indicates whether the list is empty.
+     *
+     * @return true is the list is empty, false otherwise.
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * length() returns the length of this list.
+     *
+     * @return the length of the list.
+     */
+    public int length() {
+        return size;
+    }
+
+    /**
+     * insertFront() inserts item "obj" at the beginning of this list.
+     *
+     * @param obj the item to be inserted.
+     */
+
+    public void insertFront(Object obj) {
+        this.head = new SListNode(obj, this.head);
+        this.size++;
+    }
+
+    /**
+     * insertEnd() inserts item "obj" at the end of this list.
+     *
+     * @param obj the item to be inserted.
+     */
+    public void insertEnd(Object obj) {
+        if (head == null) {
+            this.head = new SListNode(obj);
+        } else {
+            SListNode node = this.head;
+            while (node.next != null) {
+                node = node.next;
+            }
+            node.next = new SListNode(obj);
+        }
+        size++;
+    }
+
+    /**
+     * nth() returns the item at the specified position. If position < 1 or
+     * position > this.length(), null is returned. Otherwise, item at
+     * position "position" is returned. The list does not change.
+     *
+     * @param position the desired position, from 1 to length(), in the list.
+     * @return the item at the given position in the list.
+     */
+
+    public Object nth(int position) {
+        if ((position < 1) || (position > this.length()) || this.head == null) {
+            return null;
+        } else {
+            SListNode currentNode = this.head;
+            while (position > 1) {
+                currentNode = currentNode.next;
+                if (currentNode == null)
+                    return null;
+                position--;
+            }
+            return currentNode.item;
+        }
+    }
+
+
+    /**
+     * toString() converts the list to String.
+     *
+     * @return a string representation of the list.
+     */
+    @Override
+    public String toString() {
+        Object obj;
+        String result = "[ ";
+
+        SListNode currentNode = head;
+
+        while (currentNode != null) {
+            obj = currentNode.item;
+            result = result + obj.toString() + " ";
+            currentNode = currentNode.next;
+        }
+        result = result + " ]";
+        return result;
+    }
+
+    private static class SListNode {
+        public SListNode next;
+        public final Object item;
+
+        public SListNode(Object item, SListNode next) {
+            this.item = item;
+            this.next = next;
+        }
+
+        public SListNode(Object item) {
+            this(item, null);
+        }
+    }
+
+}
+
+public class SListTest {
+    @Test
+    public void testEmptyList() {
+        SList lst1 = new SList();
+        assertEquals("toString on newly constructed list failed", "[  ]", lst1.toString());
+        assertTrue("isEmpty() on newly constructed list failed", lst1.isEmpty());
+        assertEquals("length on newly constructed list failed", 0, lst1.length());
+        lst1.insertFront(3);
+        assertEquals("InsertFront on empty list failed", "[ 3  ]", lst1.toString());
+        SList lst2 = new SList();
+        lst2.insertEnd(5);
+        assertEquals("insertEnd on empty list failed", "[ 5  ]", lst2.toString());
+    }
+
+
+    /**
+     * testAfterInsertFront() tests toString(), isEmpty(), length(),
+     * insertFront(), and insertEnd() after insertFront(). Prints summary
+     * information of the tests and halts the program if errors are detected.
+     */
+    private static void testAfterInsertFront() {
+        SList lst1 = new SList();
+        lst1.insertFront(new Integer(3));
+        lst1.insertFront(new Integer(2));
+        lst1.insertFront(new Integer(1));
+        System.out.println();
+        System.out.println("Here is a list after insertFront 3, 2, 1: " + lst1.toString());
+        //        TestHelper.verify(lst1.toString().equals("[  1  2  3  ]"), "InsertFronts on non-empty list failed");
+        //        System.out.println("isEmpty() should be false. It is: " +
+        //                lst1.isEmpty());
+        //        TestHelper.verify(lst1.isEmpty() == false,
+        //                "isEmpty() after insertFront failed");
+        //        System.out.println("length() should be 3. It is: " +
+        //                lst1.length());
+        //        TestHelper.verify(lst1.length() == 3,
+        //                "length() after insertFront failed");
+        //        lst1.insertEnd(new Integer(4));
+        //        System.out.println("Here is the same list after insertEnd(4): "
+        //                + lst1.toString());
+        //        TestHelper.verify(lst1.toString().equals("[  1  2  3  4  ]"),
+        //                "insertEnd on non-empty list failed");
+    }
+
+    /**
+     * testAfterInsertEnd() tests toString(), isEmpty(), length(),
+     * insertFront(), and insertEnd() after insertEnd().  Prints summary
+     * information of the tests and halts the program if errors are detected.
+     */
+
+    private static void testAfterInsertEnd() {
+        SList lst1 = new SList();
+        lst1.insertEnd(new Integer(6));
+        lst1.insertEnd(new Integer(7));
+        System.out.println();
+        System.out.println("Here is a list after insertEnd 6, 7: " + lst1.toString());
+        System.out.println("isEmpty() should be false. It is: " + lst1.isEmpty());
+        //        TestHelper.verify(lst1.isEmpty() == false,
+        //                "isEmpty() after insertEnd failed");
+        //        System.out.println("length() should be 2. It is: " +
+        //                lst1.length());
+        //        TestHelper.verify(lst1.length() == 2,
+        //                "length() after insertEndfailed");
+        //        lst1.insertFront(new Integer(5));
+        //        System.out.println("Here is the same list after insertFront(5): "
+        //                + lst1.toString());
+        //        TestHelper.verify(lst1.toString().equals("[  5  6  7  ]"),
+        //                "insertFront after insertEnd failed");
+    }
+
+}
---
+++ java-practice/src/test/java/com/janosgyerik/stackoverflow/tim/timing/Statistics.java
@@ -0,0 +1,70 @@
+package com.janosgyerik.stackoverflow.tim.timing;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Statistics {
+
+    private static final int REMOVE_BEST_PERCENT = 10;
+    private static final int REMOVE_WORST_PERCENT = 10;
+
+    /**
+     * removes the x lowest and x highest values from the list.
+     *
+     * Also sorts the list ascending.
+     *
+     * @param list list
+     */
+    public static void removeWorstAndBest(List<Long> list) {
+        // sort ascending
+        Collections.sort(list,
+                (Long l1, Long l2) -> (int) (l1 - l2));
+
+        // remove x worst and x best results
+        int originalSize = list.size();
+        list.subList(list.size() - REMOVE_BEST_PERCENT * originalSize / 100,
+                originalSize).clear();
+        list.subList(0, REMOVE_WORST_PERCENT * originalSize / 100).clear();
+    }
+
+    /**
+     * returns the mean of the list.
+     *
+     * @param list list
+     * @return mean
+     */
+    public static double calculateMean(final List<Long> list) {
+        double mean = 0;
+        final int length = list.size();
+        for (Long item : list) {
+            mean += item / (double) length;
+        }
+        return mean;
+    }
+
+    /**
+     * returns the median of the list.
+     *
+     * Expects a sorted list.
+     *
+     * @param list list
+     * @return median
+     */
+    public static double calculateMedian(List<Long> list) {
+        return list.get(list.size() / 2);
+    }
+
+    /**
+     * returns the
+     *
+     * Expects a sorted list.
+     *
+     * @param list list
+     * @param percentile percentile
+     * @return percentile
+     */
+    public static double percentile(List<Long> list, int percentile) {
+        int rank = (int) Math.ceil(percentile / 100 * list.size());
+        return list.get(rank);
+    }
+}
