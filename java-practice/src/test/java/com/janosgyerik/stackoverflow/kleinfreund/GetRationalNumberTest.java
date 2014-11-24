f57b6bc693895894d6abc25cc0cc3af44b08d00f
Janos Gyerik <janos@kronos>
2014-11-24 21:05:15 +0100

    more answers

---
+++ java-practice/src/test/java/com/janosgyerik/stackoverflow/kleinfreund/GetRationalNumberTest.java
@@ -0,0 +1,63 @@
+package com.janosgyerik.stackoverflow.kleinfreund;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+class RationalNumber {
+    private final int numerator;
+    private final int denominator;
+
+    RationalNumber(int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new IllegalArgumentException("The denominator must be non-zero");
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString((double) numerator / denominator);
+    }
+}
+
+class GetRationalNumber {
+
+    public static void main(String[] args) {
+//        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+        Scanner userInput = new Scanner("3 4");
+//        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Numerator: ");
+        int p = userInput.nextInt();
+
+        System.out.println("Denominator: ");
+        int q = userInput.nextInt();
+
+        if (q == 0) {
+            System.out.println("Divison by zero can cause serious trouble to the universe!");
+            System.exit(0);
+        }
+
+        System.out.println("Result:\n" + (double) p / (double) q);
+        System.out.println("Result:\n" + p / (double) q);
+        System.out.println("Result:\n" + (double) p / q);
+
+        RationalNumber rationalNumber = new RationalNumber(p, q);
+        System.out.println("Result:\n" + rationalNumber);
+
+        System.out.println("Result:\n" + new RationalNumber(p, q));
+    }
+}
+
+public class GetRationalNumberTest {
+    @Test
+    public void test() throws IOException {
+        GetRationalNumber.main(new String[0]);
+    }
+}
