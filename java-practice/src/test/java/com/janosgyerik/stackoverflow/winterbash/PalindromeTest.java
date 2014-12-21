b1d7c27019b362f00791bee094d569618f6b30c9
Janos Gyerik <janos@kronos>
2014-12-21 22:30:52 +0100

    more answers

---
+++ java-practice/src/test/java/com/janosgyerik/stackoverflow/winterbash/PalindromeTest.java
@@ -0,0 +1,69 @@
+package com.janosgyerik.stackoverflow.winterbash;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PalindromeTest {
+    public boolean palCheck(String s) {
+        String end = "";
+        String result = s.replaceAll(" ", "");
+        for (int i = result.length() - 1; i >= 0; i--) {
+            end += result.charAt(i);
+        }
+
+        return result.equalsIgnoreCase(end);
+    }
+
+    private boolean checkPalindrome(String string) {
+        char[] chars = string.replaceAll(" ", "").toLowerCase().toCharArray();
+        for (int i = 0; i < chars.length / 2; ++i) {
+            if (chars[i] != chars[chars.length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String string) {
+        return checkPalindrome(string);
+    }
+
+    @Test
+    public void test_hello_is_not_palindrome() {
+        assertFalse(isPalindrome("hello"));
+    }
+
+    @Test
+    public void test_hello_olleh_is_palindrome() {
+        assertTrue(isPalindrome("hello olleh"));
+    }
+
+    @Test
+    public void test_aba_is_palindrome() {
+        assertTrue(isPalindrome("aba"));
+    }
+
+    @Test
+    public void test_x_palindrome() {
+        assertTrue(isPalindrome("x"));
+    }
+
+    @Test
+    public void test_xx_palindrome() {
+        assertTrue(isPalindrome("xx"));
+    }
+
+    @Test
+    public void test_empty_palindrome() {
+        assertTrue(isPalindrome("xax"));
+    }
+
+    @Test
+    public void test_hello() {
+        assertTrue(isPalindrome(""));
+    }
+}
