a4a1a314eb744093121bd36f3b1cf1321cf1e84b
Janos Gyerik <janos@kronos>
2015-02-13 23:15:04 +0100

    added common classes

---
+++ java-practice/src/test/java/com/janosgyerik/ojleetcode/common/ListNode.java
@@ -0,0 +1,12 @@
+package com.janosgyerik.ojleetcode.common;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
---
+++ java-practice/src/test/java/com/janosgyerik/ojleetcode/common/TreeNode.java
@@ -0,0 +1,11 @@
+package com.janosgyerik.ojleetcode.common;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
