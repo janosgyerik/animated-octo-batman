5b1e94be6f7c7644750eeb3a8ead25b063ee9496
Janos Gyerik <janos@kronos>
2015-02-13 23:33:39 +0100

    BalancedBinaryTreeTest

---
+++ java-practice/src/test/java/com/janosgyerik/ojleetcode/easy/BalancedBinaryTreeTest.java
@@ -0,0 +1,42 @@
+package com.janosgyerik.ojleetcode.easy;
+
+import com.janosgyerik.ojleetcode.common.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class BalancedBinaryTreeTest {
+    private static final class SearchInfo {
+        private final int height;
+        private final boolean balanced;
+
+        private SearchInfo(int height, boolean balanced) {
+            this.height = height;
+            this.balanced = balanced;
+        }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return isBalancedHelper(root).balanced;
+    }
+
+    private SearchInfo isBalancedHelper(TreeNode node) {
+        if (node == null) {
+            return new SearchInfo(0, true);
+        }
+        SearchInfo left = isBalancedHelper(node.left);
+        SearchInfo right = isBalancedHelper(node.right);
+        if (left.balanced && right.balanced && Math.abs(left.height - right.height) < 2) {
+            return new SearchInfo(1 + Math.max(left.height, right.height), true);
+        }
+        return new SearchInfo(-1, false);
+    }
+
+    @Test
+    public void testFromString() {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.right = new TreeNode(3);
+        assertFalse(isBalanced(root));
+    }
+}
