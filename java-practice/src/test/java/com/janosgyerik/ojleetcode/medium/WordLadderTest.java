287f6f767ef94ed2923158ace598c5a6a4a20116
Janos Gyerik <janos@kronos>
2015-02-23 12:33:06 +0100

    WordLadderTest BFS

--- java-practice/src/test/java/com/janosgyerik/ojleetcode/medium/WordLadderTest.java
+++ java-practice/src/test/java/com/janosgyerik/ojleetcode/medium/WordLadderTest.java
@@ -2,34 +2,55 @@ package com.janosgyerik.ojleetcode.medium;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class WordLadderTest {
+
     public int ladderLength(String start, String end, Set<String> dict) {
-        return ladderLength(start, end, dict, dict.size());
+        Queue<String> queue = new LinkedList<>();
+        queue.add(start);
+        int level = 2;
+        while (!queue.isEmpty()) {
+            for (String item : new LinkedList<>(queue)) {
+                if (distance(item, end) == 1) {
+                    return level;
+                }
+                queue.poll();
+                for (String word : new ArrayList<>(dict)) {
+                    if (distance(item, word) == 1) {
+                        dict.remove(word);
+                        queue.add(word);
+                    }
+                }
+            }
+            ++level;
+        }
+        return 0;
     }
 
-    public int ladderLength(String start, String end, Set<String> dict, int maxMoves) {
-        if (distance(start, end) == 1) {
-            return 2;
-        } else if (maxMoves == 0) {
-            return dict.size();
-        }
-        int shortest = dict.size() + 2;
-        for (String word : dict) {
-            int dist = distance(start, word);
-            if (dist == 1) {
-                int length = 1 + ladderLength(word, end, copyWithoutWord(dict, word), Math.min(maxMoves, shortest) - 1);
-                if (length < shortest) {
-                    shortest = length;
+    public int ladderLength2(String start, String end, Set<String> dict) {
+        Queue<List<String>> queue = new LinkedList<>();
+        queue.add(new LinkedList<>(Arrays.asList(start)));
+        while (!queue.isEmpty()) {
+            for (List<String> sequence : new LinkedList<>(queue)) {
+                String last = sequence.get(sequence.size() - 1);
+                if (distance(last, end) == 1) {
+                    return sequence.size() + 1;
+                }
+                queue.poll();
+                for (String word : new ArrayList<>(dict)) {
+                    if (distance(last, word) == 1 && !sequence.contains(word)) {
+                        dict.remove(word);
+                        List<String> copy = new LinkedList<>(sequence);
+                        copy.add(word);
+                        queue.add(copy);
+                    }
                 }
             }
         }
-        return shortest;
+        return 0;
     }
 
     private int distance(String start, String word) {
@@ -42,19 +63,20 @@ public class WordLadderTest {
         return dist;
     }
 
-    private Set<String> copyWithoutWord(Set<String> dict, String word) {
-        Set<String> copy = new HashSet<>(dict);
-        copy.remove(word);
-        return copy;
-    }
-
     @Test
     public void test_hit_cog() {
-        assertEquals(5, ladderLength("hit", "cog", new HashSet<>(Arrays.asList("hot","dot","dog","lot","log"))));
+        assertEquals(5, ladderLength("hit", "cog", new HashSet<>(Arrays.asList("hot", "dot", "dog", "lot", "log"))));
     }
 
     @Test
     public void test_long() {
-        assertEquals(5, ladderLength("qa", "sq", new HashSet<>(Arrays.asList("si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"))));
+        Set<String> dict = new HashSet<>(Arrays.asList("si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye"));
+        assertEquals(5, ladderLength("qa", "sq", dict));
+    }
+
+    @Test
+    public void test_longer() {
+        Set<String> dict = new HashSet<>(Arrays.asList("kid", "tag", "pup", "ail", "tun", "woo", "erg", "luz", "brr", "gay", "sip", "kay", "per", "val", "mes", "ohs", "now", "boa", "cet", "pal", "bar", "die", "war", "hay", "eco", "pub", "lob", "rue", "fry", "lit", "rex", "jan", "cot", "bid", "ali", "pay", "col", "gum", "ger", "row", "won", "dan", "rum", "fad", "tut", "sag", "yip", "sui", "ark", "has", "zip", "fez", "own", "ump", "dis", "ads", "max", "jaw", "out", "btu", "ana", "gap", "cry", "led", "abe", "box", "ore", "pig", "fie", "toy", "fat", "cal", "lie", "noh", "sew", "ono", "tam", "flu", "mgm", "ply", "awe", "pry", "tit", "tie", "yet", "too", "tax", "jim", "san", "pan", "map", "ski", "ova", "wed", "non", "wac", "nut", "why", "bye", "lye", "oct", "old", "fin", "feb", "chi", "sap", "owl", "log", "tod", "dot", "bow", "fob", "for", "joe", "ivy", "fan", "age", "fax", "hip", "jib", "mel", "hus", "sob", "ifs", "tab", "ara", "dab", "jag", "jar", "arm", "lot", "tom", "sax", "tex", "yum", "pei", "wen", "wry", "ire", "irk", "far", "mew", "wit", "doe", "gas", "rte", "ian", "pot", "ask", "wag", "hag", "amy", "nag", "ron", "soy", "gin", "don", "tug", "fay", "vic", "boo", "nam", "ave", "buy", "sop", "but", "orb", "fen", "paw", "his", "sub", "bob", "yea", "oft", "inn", "rod", "yam", "pew", "web", "hod", "hun", "gyp", "wei", "wis", "rob", "gad", "pie", "mon", "dog", "bib", "rub", "ere", "dig", "era", "cat", "fox", "bee", "mod", "day", "apr", "vie", "nev", "jam", "pam", "new", "aye", "ani", "and", "ibm", "yap", "can", "pyx", "tar", "kin", "fog", "hum", "pip", "cup", "dye", "lyx", "jog", "nun", "par", "wan", "fey", "bus", "oak", "bad", "ats", "set", "qom", "vat", "eat", "pus", "rev", "axe", "ion", "six", "ila", "lao", "mom", "mas", "pro", "few", "opt", "poe", "art", "ash", "oar", "cap", "lop", "may", "shy", "rid", "bat", "sum", "rim", "fee", "bmw", "sky", "maj", "hue", "thy", "ava", "rap", "den", "fla", "auk", "cox", "ibo", "hey", "saw", "vim", "sec", "ltd", "you", "its", "tat", "dew", "eva", "tog", "ram", "let", "see", "zit", "maw", "nix", "ate", "gig", "rep", "owe", "ind", "hog", "eve", "sam", "zoo", "any", "dow", "cod", "bed", "vet", "ham", "sis", "hex", "via", "fir", "nod", "mao", "aug", "mum", "hoe", "bah", "hal", "keg", "hew", "zed", "tow", "gog", "ass", "dem", "who", "bet", "gos", "son", "ear", "spy", "kit", "boy", "due", "sen", "oaf", "mix", "hep", "fur", "ada", "bin", "nil", "mia", "ewe", "hit", "fix", "sad", "rib", "eye", "hop", "haw", "wax", "mid", "tad", "ken", "wad", "rye", "pap", "bog", "gut", "ito", "woe", "our", "ado", "sin", "mad", "ray", "hon", "roy", "dip", "hen", "iva", "lug", "asp", "hui", "yak", "bay", "poi", "yep", "bun", "try", "lad", "elm", "nat", "wyo", "gym", "dug", "toe", "dee", "wig", "sly", "rip", "geo", "cog", "pas", "zen", "odd", "nan", "lay", "pod", "fit", "hem", "joy", "bum", "rio", "yon", "dec", "leg", "put", "sue", "dim", "pet", "yaw", "nub", "bit", "bur", "sid", "sun", "oil", "red", "doc", "moe", "caw", "eel", "dix", "cub", "end", "gem", "off", "yew", "hug", "pop", "tub", "sgt", "lid", "pun", "ton", "sol", "din", "yup", "jab", "pea", "bug", "gag", "mil", "jig", "hub", "low", "did", "tin", "get", "gte", "sox", "lei", "mig", "fig", "lon", "use", "ban", "flo", "nov", "jut", "bag", "mir", "sty", "lap", "two", "ins", "con", "ant", "net", "tux", "ode", "stu", "mug", "cad", "nap", "gun", "fop", "tot", "sow", "sal", "sic", "ted", "wot", "del", "imp", "cob", "way", "ann", "tan", "mci", "job", "wet", "ism", "err", "him", "all", "pad", "hah", "hie", "aim", "ike", "jed", "ego", "mac", "baa", "min", "com", "ill", "was", "cab", "ago", "ina", "big", "ilk", "gal", "tap", "duh", "ola", "ran", "lab", "top", "gob", "hot", "ora", "tia", "kip", "han", "met", "hut", "she", "sac", "fed", "goo", "tee", "ell", "not", "act", "gil", "rut", "ala", "ape", "rig", "cid", "god", "duo", "lin", "aid", "gel", "awl", "lag", "elf", "liz", "ref", "aha", "fib", "oho", "tho", "her", "nor", "ace", "adz", "fun", "ned", "coo", "win", "tao", "coy", "van", "man", "pit", "guy", "foe", "hid", "mai", "sup", "jay", "hob", "mow", "jot", "are", "pol", "arc", "lax", "aft", "alb", "len", "air", "pug", "pox", "vow", "got", "meg", "zoe", "amp", "ale", "bud", "gee", "pin", "dun", "pat", "ten", "mob"));
+        assertEquals(11, ladderLength("cet", "ism", dict));
     }
 }
