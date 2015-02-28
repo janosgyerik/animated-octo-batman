0cb7610f124a2395998decf6234d7048e8913c96
Janos Gyerik <janos@kronos>
2015-02-28 19:22:33 +0100

    SubsetsTest

---
+++ java-practice/src/test/java/com/janosgyerik/ojleetcode/medium/SubsetsTest.java
@@ -0,0 +1,45 @@
+package com.janosgyerik.ojleetcode.medium;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class SubsetsTest {
+    private static class ListWithIndex {
+        final List<Integer> list;
+        final int index;
+
+        ListWithIndex(List<Integer> list, int index) {
+            this.list = list;
+            this.index = index;
+        }
+    }
+
+    public List<List<Integer>> subsets(int[] S) {
+        List<List<Integer>> results = new LinkedList<>();
+        results.add(Arrays.asList());
+        if (S.length == 0) {
+            return results;
+        }
+
+        Arrays.sort(S);
+
+        Queue<ListWithIndex> canGrow = new LinkedList<>();
+        canGrow.add(new ListWithIndex(new LinkedList<>(), 0));
+
+        while (!canGrow.isEmpty()) {
+            ListWithIndex listWithIndex = canGrow.poll();
+            for (int i = listWithIndex.index; i < S.length - 1; ++i) {
+                List<Integer> copy = new LinkedList<>(listWithIndex.list);
+                copy.add(S[i]);
+                results.add(copy);
+                canGrow.add(new ListWithIndex(copy, i + 1));
+            }
+            List<Integer> copy = new LinkedList<>(listWithIndex.list);
+            copy.add(S[S.length - 1]);
+            results.add(copy);
+        }
+        return results;
+    }
+}
