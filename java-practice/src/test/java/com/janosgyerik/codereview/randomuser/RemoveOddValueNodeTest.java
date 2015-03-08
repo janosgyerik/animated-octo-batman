e06cb1c48ef1343c5f8834a2060540dfae71810f
Janos Gyerik <janos@kronos>
2015-03-08 15:22:26 +0100

    more answers

---
+++ java-practice/src/test/java/com/janosgyerik/codereview/randomuser/RemoveOddValueNodeTest.java
@@ -0,0 +1,65 @@
+package com.janosgyerik.codereview.randomuser;
+
+import com.janosgyerik.ojleetcode.common.ListNode;
+import com.janosgyerik.ojleetcode.common.ListNodeUtils;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RemoveOddValueNodeTest {
+    @Test
+    public void test1() {
+        ListNode head = ListNodeUtils.deserialize("[1,2,3,4,5,6,4,4,4,7,8]");
+        assertEquals("[2,4,6,4,4,4,8]", ListNodeUtils.serialize(removeOdd(head)));
+    }
+
+    @Test
+    public void test2() {
+        ListNode head = ListNodeUtils.deserialize("[1,1,1,1,2,3,4,5,6,4,4]");
+        assertEquals("[2,4,6,4,4]", ListNodeUtils.serialize(removeOdd(head)));
+    }
+
+    @Test
+    public void test3() {
+        ListNode head = ListNodeUtils.deserialize("[1,1,1,1,3,5,9]");
+        assertEquals("[]", ListNodeUtils.serialize(removeOdd(head)));
+    }
+
+    @Test
+    public void test4() {
+        ListNode head = ListNodeUtils.deserialize("[2,4,6]");
+        assertEquals("[2,4,6]", ListNodeUtils.serialize(removeOdd(head)));
+    }
+
+    @Test
+    public void test5() {
+        ListNode head = ListNodeUtils.deserialize("[2]");
+        assertEquals("[2]", ListNodeUtils.serialize(removeOdd(head)));
+    }
+
+    @Test
+    public void test6() {
+        ListNode head = ListNodeUtils.deserialize("[2,3,3,5]");
+        assertEquals("[2]", ListNodeUtils.serialize(removeOdd(head)));
+    }
+
+    private ListNode removeOdd(ListNode head) {
+        while (head != null && head.val % 2 != 0) {
+            head = head.next;
+        }
+        if (head == null) {
+            return null;
+        }
+        ListNode lastEven = head;
+        ListNode current = head.next;
+        lastEven.next = null;
+        while (current != null) {
+            if (current.val % 2 == 0) {
+                lastEven.next = current;
+                lastEven = current;
+            }
+            current = current.next;
+        }
+        return head;
+    }
+}
